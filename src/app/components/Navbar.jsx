d4bce3e9941bfac175a65d5bc01142d73cf6f93a
AnilVarma-G <ristoglobaltech.md@gmail.com>
2024-06-20 21:55:37 +0530

    add

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -211,7 +211,7 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed w-full border lg:h-24 h-20 rounded-sm top-0 left-0 right-0 z-10 bg-pink-200 bg-opacity-100">
+    <nav className="fixed w-full border h-20 rounded-sm top-0 left-0 right-0 z-10 bg-pink-200 bg-opacity-100">
       <div className="container flex flex-wrap items-center justify-between mx-auto px-4 py-2 lg:py-4">
         <Link href="/" className="flex items-center text-[15px] md:text-4xl lg:text-5xl font-semibold overflow-hidden">
           <span
